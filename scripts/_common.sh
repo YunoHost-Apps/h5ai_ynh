d69266d57431a77622cfc3e7ce23907c34dd67c4
ericgaspar <junk.eg@free.fr>
2022-10-16 20:38:07 +0200

    Fix script order

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,9 @@
 YNH_PHP_VERSION=7.4
 
 # dependencies used by the app
-pkg_dependencies="php$YNH_PHP_VERSION-fpm ffmpeg imagemagick"
+php_dependencies="php$YNH_PHP_VERSION-fpm"
+
+pkg_dependencies="ffmpeg imagemagick $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
