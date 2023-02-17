81adda6b198a8e02df13978627272dcacc66b151
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-17 22:45:03 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,12 +6,7 @@
 # PHP APP SPECIFIC
 #=================================================
 
-YNH_PHP_VERSION=7.4
-
-#REMOVEME? php_dependencies="php$YNH_PHP_VERSION-fpm"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="ffmpeg imagemagick $php_dependencies"
+YNH_PHP_VERSION=8.0
 
 #=================================================
 # PERSONAL HELPERS
