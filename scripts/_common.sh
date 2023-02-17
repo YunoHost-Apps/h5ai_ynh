c4d68672a4c59e2ee57833a217c0d4b977028cfb
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-17 22:34:43 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,10 +8,10 @@
 
 YNH_PHP_VERSION=7.4
 
-php_dependencies="php$YNH_PHP_VERSION-fpm"
+#REMOVEME? php_dependencies="php$YNH_PHP_VERSION-fpm"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="ffmpeg imagemagick $php_dependencies"
+#REMOVEME? pkg_dependencies="ffmpeg imagemagick $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
